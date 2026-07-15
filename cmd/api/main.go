99b336fd081412841eec1ad47479e00c3115434b
Miguel de Almeida Alves <migueldealmeidaalves55@gmail.com>
2026-07-15 09:03:35 -0300

    Implemented graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/Alvesafk/blog-2/internal/db"
 	"github.com/Alvesafk/blog-2/internal/handlers"
@@ -27,14 +31,35 @@ func main() {
 	}
 	defer database.Close()
 
-	srv := handlers.NewServer(database)
+	con := handlers.NewConnection(database)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /post", srv.GetPosts)
-	mux.HandleFunc("GET /post/{id}", srv.GetPost)
-	mux.HandleFunc("GET /comment/{id}", srv.GetComments)
+	mux.HandleFunc("GET /post", con.GetPosts)
+	mux.HandleFunc("GET /post/{id}", con.GetPost)
+	mux.HandleFunc("GET /comment/{id}", con.GetComments)
 
 	middlewareMux := mw.SecurityHeadersMiddleware(mw.LoggingMiddleware(mux))
 
-	http.ListenAndServe(":8080", middlewareMux)
+	srv := &http.Server{
+		Addr:              ":8080",
+		Handler:           middlewareMux,
+		ReadTimeout:       5 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       120 * time.Second,
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+
+	go func(){
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("error in booting the server: %s", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	srv.Shutdown(ctx)
 }
