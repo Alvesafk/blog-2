db5e0c1f5aeb7169c5af7047785e1b3987ced0e3
Miguel de Almeida Alves <migueldealmeidaalves55@gmail.com>
2026-07-16 20:14:58 -0300

    All Posts page.

---
+++ frontend/src/modules/PostCard.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router';
+import Tags from './Tags.jsx';
+
+export default function PostCard({ post }) {
+	const date = new Date(post.postedAt).toLocaleString()
+
+	return (
+		<div>
+			<div className="post-card-header">
+				<h2>{post.title}</h2>
+				<span><small>{date}</small></span>
+			</div>
+			<div className="post-card-content">
+				<p>{post.preview}</p>
+				<Link to={"/posts/" + post.slug_title} state={{ post_id: post.id }}>Read it!</Link> <br />
+				<Tags tags={post.tags} />
+			</div>
+		</div>
+	)
+}
--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,7 +1,43 @@
+import { useState, useEffect } from 'react';
+import PostCard from '../modules/PostCard';
+
 export default function Posts() {
+	const [posts, setPosts] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		fetch(`/api/post`)
+			.then(response => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw response;
+			})
+			.then(allPosts => {
+				console.log(allPosts)
+				setPosts(allPosts);
+				setLoading(false);
+			})
+			.catch(err => {
+				console.error(`Error on fetching latest post: `, err);
+				setLoading(false)
+			});
+	}, []);
+
 	return (
 		<>
 			<h2>This is the Posts</h2>
+			{loading ? (
+				<p>Loading...</p>
+			) : posts ? (
+				<div className="post-list">
+					{posts.content.map(post =>
+						<PostCard post={post} />
+					)}
+				</div>
+			) : (
+				<p>No post found!</p>
+			)}
 		</>
 	);
 }
