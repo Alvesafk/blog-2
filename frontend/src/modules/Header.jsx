c8c784afaa290f3acff0f190627564ce3eca5d14
Miguel de Almeida Alves <migueldealmeidaalves55@gmail.com>
2026-07-15 23:00:22 -0300

    Proper linking to other pages in header

--- frontend/src/modules/Header.jsx
+++ frontend/src/modules/Header.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router";
+
 export default function Header() {
 	return (
 		<header>
 			<h1>Alves's Blog</h1>
 
 			<nav>
-				<p>Home</p>
-				<p>Posts</p>
-				<p>About</p>
-				<p>Resources</p>
-				<p>Guestbook</p>
+				<Link to="/">Home</Link>
+				<Link to="/posts">Posts</Link>
+				<Link to="/about">About</Link>
+				<Link to="/resources">Resources</Link>
+				<Link to="/guestbook">Guestbook</Link>
 			</nav>
 		</header>
 	);
