21a2a18b52173b1fdca0127ae24a6e19f8b8f01e
Miguel de Almeida Alves <migueldealmeidaalves55@gmail.com>
2026-07-23 16:22:10 -0300

    Uses a box class instead of notification one.

--- frontend/src/modules/Greet.jsx
+++ frontend/src/modules/Greet.jsx
@@ -1,16 +1,18 @@
 export default function Greet() {
 	// TODO :: find a insane pic
 	return (
-		<div className="container has-border my-2">
-			<div className="notification">
-				<div className="has-b-border">
+		<section className="container my-2">
+			<div className="box">
+				<header className="has-b-border">
 					<h2 className="is-size-5"><small className="is-size-7">wows...</small> Homepage</h2>
+				</header>
+				<div className="mt-2">
+					<img src="#" alt="INSANE PIC" />
+
+					<p>Hello!!! This my personal site/blog, you can find posts about what i been doing, you can always comment on them, leave a message at the guestbook as well!</p>
 				</div>
 
-				<img src="#" alt="INSANE PIC" />
-
-				<p>Hello!!! This my personal site/blog, you can find posts about what i been doing, you can always comment on them, leave a message at the guestbook as well!</p>
 			</div>
-		</div>
+		</section>
 	)
 }
