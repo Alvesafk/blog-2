64ac8c4fcabb6340d30d24d7f73c1f30a6cf2763
Miguel de Almeida Alves <migueldealmeidaalves55@gmail.com>
2026-07-23 14:59:00 -0300

    Footer base css.

--- frontend/src/modules/Footer.jsx
+++ frontend/src/modules/Footer.jsx
@@ -1,10 +1,12 @@
 export default function Footer() {
 	return (
-		<footer>
-			<p>
-				<small>beep... beep...</small>
-				<a href="https://github.com/Alvesafk" target="_blank">alvesafk</a> @ 2026
-			</p>
+		<footer className="footer">
+			<div className="content has-text-centered">
+				<p>
+					<small>beep... beep...</small>
+					<a href="https://github.com/Alvesafk" target="_blank">alvesafk</a> @ 2026
+				</p>
+			</div>
 		</footer>
 	);
 }
