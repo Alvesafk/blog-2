e61d802c869383ad31b1a28071b95455db78606c
Miguel de Almeida Alves <migueldealmeidaalves55@gmail.com>
2026-08-05 22:39:59 -0300

    Used CPU percentage.

--- backend/internal/serverstats/server.go
+++ backend/internal/serverstats/server.go
@@ -3,18 +3,21 @@ package serverstats
 import (
 	"runtime"
 
+	"github.com/shirou/gopsutil/v4/cpu"
 	"github.com/shirou/gopsutil/v4/mem"
 )
 
 type ServerStats struct {
-	Goroutines int `json:"goroutines"`
+	Goroutines int     `json:"goroutines"`
 	UsedMemory float64 `json:"usedMemory"`
+	UsedCPU    float64 `json:"usedCPU"`
 }
 
 func NewServerStats() *ServerStats {
 	return &ServerStats{
 		Goroutines: runtime.NumGoroutine(),
 		UsedMemory: getUsedMemory(),
+		UsedCPU:    getUsedCPU(),
 	}
 }
 
@@ -25,4 +28,18 @@ func getUsedMemory() float64 {
 	}
 
 	return mem.UsedPercent
+}
+
+func getUsedCPU() float64 {
+	cpuSlice, err := cpu.Percent(0, true)
+	if err != nil {
+		return -1
+	}
+
+	var total float64
+	for _, v := range cpuSlice {
+		total += v
+	}
+
+	return total / float64(len(cpuSlice))
 }
