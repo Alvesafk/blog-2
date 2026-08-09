24c9443940367658a7594daa26b7d43ff79f2ee4
Miguel de Almeida Alves <migueldealmeidaalves55@gmail.com>
2026-08-08 21:09:43 -0300

    Basic handlers.go, Connection and Response structs.

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"gorm.io/gorm"
+)
+
+type Connection struct {
+	DB *gorm.DB
+}
+
+func NewConnection(db *gorm.DB) *Connection {
+	return &Connection{db}
+}
+
+type Response struct {
+	Message string `json:"message"`
+	Content any    `json:"content"`
+}
+
+func (r Response) WriteJSON(w http.ResponseWriter, h int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(h)
+
+	enc := json.NewEncoder(w)
+	enc.SetEscapeHTML(false)
+	enc.Encode(r)
+}
+
+func (c Connection) GetHelloWorld(w http.ResponseWriter, r *http.Request) {
+	Response{
+		Message: "This is a hello world.",
+		Content: "Hello, World!",
+	}.WriteJSON(w, http.StatusOK)
+}
