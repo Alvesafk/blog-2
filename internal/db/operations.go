131dbad80afe5362c9c6075498a52f500fbd4151
Miguel de Almeida Alves <migueldealmeidaalves55@gmail.com>
2026-08-09 20:01:35 -0300

    Added comments operations on the DB.

--- internal/db/operations.go
+++ internal/db/operations.go
@@ -65,8 +65,8 @@ func GetMostRecentPost(db *gorm.DB) (*models.Post, error) {
 	return &post, nil
 }
 
-func UpdatePost(db *gorm.DB, oldPostID int, updatedPost models.Post) (*models.Post, error) {
-	post, err := GetPostByID(db, oldPostID)
+func UpdatePost(db *gorm.DB, postID int, updatedPost models.Post) (*models.Post, error) {
+	post, err := GetPostByID(db, postID)
 	if err != nil {
 		return nil, err
 	}
@@ -92,6 +92,82 @@ func DeletePostByID(db *gorm.DB, postID int) error {
 	result := db.Delete(&models.Post{}, postID)
 	if result.Error != nil {
 		return result.Error
+	}
+
+	return nil
+}
+
+func CreateComment(db *gorm.DB, content, author string, postID int) (int, error) {
+	// Checking if post exists.
+	var post models.Post
+	if err := db.First(&post, postID).Error; err != nil {
+		return -1, err
+	}
+
+	comment := models.Comment{
+		Content: content,
+		Author:  author,
+		PostID:  uint(postID),
+	}
+
+	result := db.Create(&comment)
+	if result.Error != nil {
+		return -1, result.Error
+	}
+
+	return int(result.RowsAffected), nil
+}
+
+func GetCommentsByPostID(db *gorm.DB, postID int) ([]models.Comment, error) {
+	// Checking if post exists.
+	var post models.Post
+	if err := db.First(&post, postID).Error; err != nil {
+		return nil, err
+	}
+
+	var comments []models.Comment
+	if err := db.Where("post_id = ?", postID).Find(&comments).Error; err != nil {
+		return nil, err
+	}
+
+	return comments, nil
+}
+
+func GetCommentByID(db *gorm.DB, id int) (*models.Comment, error) {
+	var comment models.Comment
+
+	if err := db.First(&comment, "id = ?", "int_primary_key").Error; err != nil {
+		return nil, err
+	}
+
+	return &comment, nil
+}
+
+func UpdateComment(db *gorm.DB, commentID int, updatedComment models.Comment) (*models.Comment, error) {
+	comment, err := GetCommentByID(db, commentID)
+	if err != nil {
+		return nil, err
+	}
+
+	result := db.Model(&comment).Updates(updatedComment)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &updatedComment, nil
+}
+
+func DeleteComment(db *gorm.DB, comment models.Comment) error {
+	if err := db.Delete(&comment).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DeleteCommentByID(db *gorm.DB, commentID int) error {
+	if err := db.Delete(&models.Comment{}, commentID).Error; err != nil {
+		return err
 	}
 
 	return nil
